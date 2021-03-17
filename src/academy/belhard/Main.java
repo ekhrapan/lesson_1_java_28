f8baf5886e60790e8def9b06362c738299a272c9
ekhrapan <yauheni.khrapan@gmail.com>
2021-03-17 20:13:55 +0300

    Третий комит

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -3,6 +3,10 @@ package academy.belhard;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (true) {
+            System.out.println("Hello world!");
+        } else {
+            System.out.println("Bye world!");
+        }
     }
 }
